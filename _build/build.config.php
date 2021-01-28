fc031f3e64dbc8998b50d1eb967eaf7a7429f607
madaboutbrighton <mail@madaboutbrighton.net>
2021-01-28 14:20:36 +0000

    Update to v1.4.0-pl

--- _build/build.config.php
+++ _build/build.config.php
@@ -7,7 +7,7 @@ define('PKG_NAME_LOWER', strtolower(PKG_NAME));
 //define('PKG_BUILD_DEV', 'DEV');
 
 define('PKG_VERSION','1.4.0');
-define('PKG_RELEASE_TYPE', 'dev');
+define('PKG_RELEASE_TYPE', 'pl');
 
 /* define build options */
 //define('BUILD_MENU_UPDATE', false);
